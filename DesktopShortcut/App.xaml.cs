0314243ddb4a83e5f267ba56538bc1c6db23ba8e
黄钊 <my.world.hz@hotmail.com>
2020-11-10 15:26:08 +0800

    * 添加程序图标 * 修改程序集名称 * 新增异常提示

--- DesktopShortcut/App.xaml.cs
+++ DesktopShortcut/App.xaml.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Threading;
 using IWshRuntimeLibrary;
 
 namespace DesktopShortcut
@@ -19,14 +20,29 @@ namespace DesktopShortcut
     {
         protected override void OnStartup(StartupEventArgs e)
         {
+            DispatcherUnhandledException +=OnDispatcherUnhandledException;
             base.OnStartup(e);
             Create(ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None));
+        }
+
+        private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            var message = e.Exception?.Message;
+            var caption = GetType().Assembly.GetName().Name;
+            var button = MessageBoxButton.OK;
+            var icon = MessageBoxImage.Error;
+            MessageBox.Show(message, caption, button, icon);
         }
 
         private void Create(Configuration configuration)
         {
             var settings = configuration.AppSettings.Settings;
             var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            var applicationPathKey = "applicationPath";
+            if (!settings.AllKeys.Contains(applicationPathKey))
+            {
+                throw new Exception("请配置应用程序路径");
+            }
             var applicationPath = Path.Combine(baseDirectory, settings["applicationPath"].Value);
             var applicationConfiguration = ConfigurationManager.OpenExeConfiguration(applicationPath);
             var applicationName = applicationConfiguration.AppSettings.Settings["applicationName"].Value;
