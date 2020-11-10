16ece0e3189461539d3c62d3a5cbab24fc6baf7f
黄钊 <my.world.hz@hotmail.com>
2020-11-10 15:03:38 +0800

    添加项目文件。

---
+++ DesktopShortcut/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using IWshRuntimeLibrary;
+
+namespace DesktopShortcut
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Create(ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None));
+        }
+
+        private void Create(Configuration configuration)
+        {
+            var settings = configuration.AppSettings.Settings;
+            var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            var applicationPath = Path.Combine(baseDirectory, settings["applicationPath"].Value);
+            var applicationConfiguration = ConfigurationManager.OpenExeConfiguration(applicationPath);
+            var applicationName = applicationConfiguration.AppSettings.Settings["applicationName"].Value;
+            var description = settings["description"].Value;
+            var desktopDirectory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            var shortcutPath = Path.Combine(desktopDirectory, $"{applicationName}.lnk");
+            var shell = new WshShell();
+            var shortcut = (IWshShortcut) shell.CreateShortcut(shortcutPath);
+            shortcut.TargetPath = applicationPath;
+            shortcut.WorkingDirectory = Path.GetDirectoryName(applicationPath);
+            shortcut.WindowStyle = 1;
+            shortcut.Description = description;
+            shortcut.Save();
+        }
+    }
+}
---
+++ DesktopShortcut/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopShortcut
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly FadeManager manager = new FadeManager()
+        {
+            In = 1,
+            Out = 0,
+            MillisecondsDelay = 1000
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            manager.FadeIn(this, FadeIn_Completed);
+        }
+
+        private async void FadeIn_Completed(object sender, EventArgs e)
+        {
+            await Task.Delay(1000);
+            manager.FadeOut(this, FadeOut_Completed);
+        }
+
+        private void FadeOut_Completed(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+
+    public class FadeManager
+    {
+        private readonly FillBehavior behavior = FillBehavior.Stop;
+        public double MillisecondsDelay { get; set; }
+        public double In { get; set; }
+        public double Out { get; set; }
+
+        public void FadeIn(UIElement element, EventHandler completed)
+        {
+            var animation = new DoubleAnimation()
+            {
+                From = Out,
+                To = In,
+                Duration = TimeSpan.FromMilliseconds(MillisecondsDelay),
+                FillBehavior = behavior,
+            };
+            animation.Completed += completed;
+            element.BeginAnimation(UIElement.OpacityProperty, animation);
+        }
+
+        public void FadeOut(UIElement element, EventHandler completed)
+        {
+            var animation = new DoubleAnimation()
+            {
+                From = In,
+                To = Out,
+                Duration = TimeSpan.FromMilliseconds(MillisecondsDelay),
+                FillBehavior = behavior,
+            };
+            animation.Completed += completed;
+            element.BeginAnimation(UIElement.OpacityProperty, animation);
+        }
+    }
+}
